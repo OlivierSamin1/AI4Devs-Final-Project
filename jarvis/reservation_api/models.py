50d954ac499eaa75bad8dc00ca17e31283671c86
Olivier <oliviersamin@gmail.com>
2025-04-05 21:59:50 +0200

    add missing import

--- jarvis/reservation_api/models.py
+++ jarvis/reservation_api/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+
+
 class Asset(models.Model):
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='real_estate_asset', blank=True, null=True)
